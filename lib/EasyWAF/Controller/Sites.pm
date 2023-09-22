ae84fb1634c55c265093da9c3f7fe7fd84031b7c
yarivha <yariv.hakim@gmail.com>
2023-09-22 11:27:34 -0400

    Add sites template

--- lib/EasyWAF/Controller/Sites.pm
+++ lib/EasyWAF/Controller/Sites.pm
@@ -4,9 +4,26 @@ use Mojo::Base 'Mojolicious::Controller', -signatures;
 
 sub view ($self) {
 
+  my $result="";
+  my $msg=""; 
+  my %sites;
+  my $sites_dir="/etc/nginx/conf.d";
+  
   $self->render(template => 'easywaf/sites',
 	        username => 'admin',
-  		title => 'Sites');
+  		title => 'Site Managment',
+		result => $result,
+		msg => $msg,
+		sites => \%sites);
+
+}
+
+sub get_sites
+{
+
+ my %sites;
+ $sites{'test1'}=["sdf","sdfsdfsdf","sfdsdfsdf","sfdsdfsfd"];
+ return (%sites);
 
 }
 
