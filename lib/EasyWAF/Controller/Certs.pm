c78d439adc62977b5b90f2867695432e8532565e
yarivha <yariv.hakim@gmail.com>
2023-09-28 16:33:40 -0400

    save certificate

--- lib/EasyWAF/Controller/Certs.pm
+++ lib/EasyWAF/Controller/Certs.pm
@@ -1,10 +1,12 @@
 package EasyWAF::Controller::Certs;
 use lib '.';
 use Mojo::Base 'Mojolicious::Controller', -signatures;
+use File::Basename 'basename';
+use File::Path 'mkpath';
 
 my $msg;
 my $result;
-my $cert_dir="/etc/nginx/certs";
+my $CERT_DIR="/etc/nginx/certs";
 my %certs;
 
 sub view ($self) {
@@ -18,6 +20,20 @@ sub view ($self) {
   $self->stash(username => $username,
                title => 'Certificate Managment',
                url => '/certs');
+  use File::Basename 'basename';
+  use File::Path 'mkpath';
+
+  # Create directory if not exists
+  unless (-d $CERT_DIR) {
+    mkpath $CERT_DIR;
+  }
+
+#---------- Create Cert ---------  
+  if ($action eq "createcert") {
+   createcert($self);
+  }
+
+
 
 #---------- Create Cert Menu ---------  
   if ($action eq "createcertmenu") {
@@ -41,6 +57,43 @@ sub get_certs
 {
  my %certs;
  return (%certs);
+}
+
+######### createcert #########
+sub createcert($self) {
+
+ my $rc;
+ my $name = $self->param("name");
+ my @cert = $self->param("cert");
+ my @key = $self->param("key");
+
+ if ($name eq "") {
+   $result="failed";
+   $msg="Certificate name was not declared";
+   return;
+ }
+
+
+ open (FILEHANDLE, ">$CERT_DIR/$name.cert");
+ print FILEHANDLE @cert;
+ close FILEHANDLE;
+
+ open (FILEHANDLE, ">$CERT_DIR/$name.key");
+ print FILEHANDLE @key;
+ close FILEHANDLE;
+
+
+ if ((-e "$CERT_DIR/$name.cert") && (-e "$CERT_DIR/$name.key")) {
+
+   $result="success";
+   $msg="Certificate $name was saved";
+ }
+ else {
+   $result="failed";
+   $msg="Certificate $name failed to save";
+ }
+ return; 
+
 }
 
 
