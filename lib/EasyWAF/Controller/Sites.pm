3f6fdf9c9ebeb9c82eccfe7aabcc9de116e4f898
yarivha <yariv.hakim@gmail.com>
2023-10-09 13:46:02 -0400

    fix basic protection in sites

--- lib/EasyWAF/Controller/Sites.pm
+++ lib/EasyWAF/Controller/Sites.pm
@@ -65,30 +65,29 @@ sub create_site($self)
 
  my $file=$SITE_DIR."/".$name.".conf";
  my $log="/var/log/nginx/".$name.".log";
-
+ 
  if (-e $file) {
   $result="failed";
   $msg="Site name already exist";	 
   return;
  }
 
-
- `/bin/echo "#### $name ####" | /usr/bin/sudo /usr/bin/tee -a $file`;
+ `/bin/echo "#### $name ####" | /usr/bin/sudo /usr/bin/tee $file`;
  `/bin/echo "server {" | /usr/bin/sudo /usr/bin/tee -a $file`;
  `/bin/echo "   listen $port;" | /usr/bin/sudo /usr/bin/tee -a $file`;
  `/bin/echo "   server_name $server;" | /usr/bin/sudo /usr/bin/tee -a $file`;
  `/bin/echo "   access_log $log;" | /usr/bin/sudo /usr/bin/tee -a $file`;
  if ($protection1) {
-  `/bin/echo "   add_header Strict-Transport-Security \"max-age=63072000; includeSubDomains; preload\";" | /usr/bin/sudo /usr/bin/tee -a $file`;
+  `/bin/echo '   add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" always;' | /usr/bin/sudo /usr/bin/tee -a $file`;
  }
  if ($protection2) {
-  `/bin/echo "   add_header X-Frame-Options DENY;" | /usr/bin/sudo /usr/bin/tee -a $file`;
+  `/bin/echo '   add_header X-Frame-Options DENY;' | /usr/bin/sudo /usr/bin/tee -a $file`;
  }
  if ($protection3) {
-  `/bin/echo "   add_header X-Content-Type-Options nosniff;" | /usr/bin/sudo /usr/bin/tee -a $file`;
+  `/bin/echo '   add_header X-Content-Type-Options nosniff;' | /usr/bin/sudo /usr/bin/tee -a $file`;
  }
  if ($protection4) {
-  `/bin/echo "   add_header X-XSS-Protection \"1; mode=block\";" | /usr/bin/sudo /usr/bin/tee -a $file`;
+  `/bin/echo '   add_header X-XSS-Protection "1; mode=block";' | /usr/bin/sudo /usr/bin/tee -a $file`;
  }
  `/bin/echo "   location / {" | /usr/bin/sudo /usr/bin/tee -a $file`;
  `/bin/echo "     proxy_pass $target;" | /usr/bin/sudo /usr/bin/tee -a $file`;
